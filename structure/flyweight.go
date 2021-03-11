6450acdae5144ba37d9628c6de751ca7ca960864
Jayden <jayden.yang@anker.com>
2021-03-11 21:12:16 +0800

    add flyweight

--- structure/flyweight.go
+++ structure/flyweight.go
@@ -1,1 +1,96 @@
 package structure
+
+import (
+	"errors"
+)
+
+type terroristDress struct { //恐怖服装
+	color string
+}
+
+func (t *terroristDress) getColor() string {
+	return t.color
+}
+
+func newTerroristDress() *terroristDress {
+	return &terroristDress{color: "red"}
+}
+
+type counterTerroristDress struct { //反恐服装
+	color string
+}
+
+func (c *counterTerroristDress) getColor() string {
+	return c.color
+}
+
+func newCounterTerroristDress() *counterTerroristDress {
+	return &counterTerroristDress{color: "green"}
+}
+
+type dress interface {
+	getColor() string
+}
+
+type dressFactory struct {
+	dressMap map[string]dress
+}
+
+const (
+	TerroristDressType        = "t"
+	CounterTerroristDressType = "ct"
+)
+
+var dressFactorySingleInstance = &dressFactory{dressMap: make(map[string]dress)}
+
+func getDressFactorySingleInstance() *dressFactory {
+	return dressFactorySingleInstance
+}
+func (d *dressFactory) getDressByType(dressType string) (dress, error) {
+	if dt, ok := d.dressMap[dressType]; ok {
+		return dt, nil
+	}
+	if dressType == TerroristDressType {
+		d.dressMap[dressType] = newTerroristDress()
+		return d.dressMap[dressType], nil
+	}
+	if dressType == CounterTerroristDressType {
+		d.dressMap[dressType] = newCounterTerroristDress()
+		return d.dressMap[dressType], nil
+	}
+	return nil, errors.New("params error")
+}
+
+type player struct {
+	dress      dress  // 服装
+	playerType string // 可为 T 或 CT
+	lat        int
+	long       int
+}
+
+func newPlayer(playerType, dressType string) *player {
+	dress, _ := getDressFactorySingleInstance().getDressByType(dressType)
+	return &player{playerType: playerType, dress: dress}
+}
+
+func (p *player) newLocation(lat, long int) {
+	p.lat = lat
+	p.long = long
+}
+
+type game struct {
+	Terrorists []*player
+	CounterTerrorists []*player
+}
+
+func newGame() *game {
+	return &game{}
+}
+
+func (g *game) addTerrorist(dressType string )  {
+	g.Terrorists = append(g.Terrorists, newPlayer(dressType,dressType))
+}
+func (g *game) addCounterTerrorist(dressType string )  {
+	g.CounterTerrorists = append(g.CounterTerrorists, newPlayer(dressType,dressType))
+}
+
---
+++ structure/flyweight_test.go
@@ -0,0 +1,28 @@
+package structure
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFlyWeight(t *testing.T) {
+
+	game := newGame()
+
+	//Add Terrorist
+	game.addTerrorist(TerroristDressType)
+	game.addTerrorist(TerroristDressType)
+	game.addTerrorist(TerroristDressType)
+	game.addTerrorist(TerroristDressType)
+
+	//Add CounterTerrorist
+	game.addCounterTerrorist(CounterTerroristDressType)
+	game.addCounterTerrorist(CounterTerroristDressType)
+	game.addCounterTerrorist(CounterTerroristDressType)
+
+	dressFactoryInstance := getDressFactorySingleInstance()
+
+	for dressType, dress := range dressFactoryInstance.dressMap {
+		fmt.Printf("DressColorType: %s\nDressColor: %s\n", dressType, dress.getColor())
+	}
+}
